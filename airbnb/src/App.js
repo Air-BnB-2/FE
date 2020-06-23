de513e6a3c680fdca1b5219aa82bb374ae8d0850
Brandon Neil <brandonneil@Brandons-MacBook-Pro.local>
2020-06-23 14:55:44 -0600

    initial commit push to new login component branch

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Header from './components/Header';
+import Header from './Components/Header';
 
 const WrapperDiv = styled.div`
   font-family: sans-serif;
---
+++ airbnb/src/components/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+import {useHistory} from 'react-router-dom';
+
+function Login () {
+
+}
+
+export default Login;
