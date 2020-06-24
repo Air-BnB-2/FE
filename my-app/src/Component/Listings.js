f70d88f99d01e786e28a15d179f571ead3df5ca1
Wayne <wabbruscatoorr@gmail.com>
2020-06-23 20:57:15 -0400

    adding notes

--- my-app/src/Component/Listings.js
+++ my-app/src/Component/Listings.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+//import CONTEXT into here
 
 export const Listings = props => {
   return (
+    // NOT SURE IF THIS WILL BE A PLACE TO DELETE A LISTING..?
+    // axiosWithAuth()
+    // .delete("")
+    // .then(res)
+    // .catch(err => console.log(err))
     <>
       <h1>Listings</h1>
       <p>{props.name}</p>
