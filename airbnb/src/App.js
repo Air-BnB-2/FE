5e30e3183a95b46e2e21af6a1cbfc309cced82b2
Brandon Neil <brandonneil@Brandons-MacBook-Pro.local>
2020-06-23 23:13:34 -0600

    login page works, having trouble with the styling, need to figure out how to get the Login submit button from sticking to the side of the page, flexbox does not work on this styled component for some reason. last commit for the night, ill come back to this tomorrow

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import styled from 'styled-components';
-import Header from './Components/Header';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import Login from './Components/Login';
+import React from "react";
+import styled from "styled-components";
+import Header from "./Components/Header";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./Components/Login";
 
 const WrapperDiv = styled.div`
   font-family: sans-serif;
   text-align: center;
 `;
 
-
 function App() {
-
   return (
     <Router>
-    <WrapperDiv>
-        <Header/>
-        <br/>
-        <Login/>
+      <WrapperDiv>
+        <Header />
+        <br />
+        <Login />
         {/*<Route></Route>*/}
-      
-    </WrapperDiv>
+      </WrapperDiv>
     </Router>
   );
 }
--- airbnb/src/components/Login.js
+++ airbnb/src/components/Login.js
@@ -1,107 +1,127 @@
-import React, {useEffect, useState} from 'react';
-import styled from 'styled-components';
-import * as yup from 'yup';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import * as yup from "yup";
+import axios from "axios";
 
-function Login (props) {
-    const {values} = props;
+const StyledForm = styled.form`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
 
-    const initialFormValues = {
-        username: '',
-        password: ''
-      }
-    
-      const [users, setUsers] = useState([])
-      const [formState, setFormState] = useState(initialFormValues)
-      const [buttonDisabled, setButtonDisabled] = useState(true)
-      const [formErrors, setFormErrors] = useState(initialFormValues)
+const SubmitButton = styled.button`
+  height: 20px;
+  width: 60px;
+  display: flex;
+  justify-content: center;
+`;
 
-      // validation
-      const formSchema = yup.object().shape({
-          username: yup
-          .string()
-          .min(3, "username must be longer than 3 characters")
-          .required("Username is a required field"),
-          password: yup
-          .string()
-          .min(3, "password must be longer than 3 characters")
-          .required("Password is a required field")
+function Login(props) {
+  const { values } = props;
+
+  const initialFormValues = {
+    username: "",
+    password: "",
+  };
+
+  const [users, setUsers] = useState([]);
+  const [formState, setFormState] = useState(initialFormValues);
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+  const [formErrors, setFormErrors] = useState(initialFormValues);
+
+  // validation
+  const formSchema = yup.object().shape({
+    username: yup
+      .string()
+      .min(3, "username must be longer than 3 characters")
+      .required("Username is a required field"),
+    password: yup
+      .string()
+      .min(3, "password must be longer than 3 characters")
+      .required("Password is a required field"),
+  });
+
+  useEffect(() => {
+    formSchema.isValid(formState).then((valid) => {
+      setButtonDisabled(!valid);
+    });
+  }, [formState]);
+
+  const validateChange = (event) => {
+    yup
+      .reach(formSchema, event.target.name)
+      .validate(event.target.value)
+      .then((valid) => {
+        setFormErrors({
+          ...formErrors,
+          [event.target.name]: "",
+        });
       })
+      .catch((error) => {
+        setFormErrors({
+          ...formErrors,
+          [event.target.name]: error.formErrors,
+        });
+      });
+  };
 
-      useEffect(() => {
-          formSchema.isValid(formState)
-            .then((valid) => {
-                setButtonDisabled(!valid)
-            });
-      }, [formState])
+  const formSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("https://reqres.in/api/users", formState)
+      .then((response) => {
+        setUsers([...users, response.data]);
+        console.log(response.data);
 
-    const validateChange = (event) => {
-        yup
-            .reach(formSchema, event.target.name)
-            .validate(event.target.value)
-            .then((valid) => {
-                setFormErrors({
-                    ...formErrors,
-                    [event.target.name]: ''
-                });
-            })
-            .catch((error) => {
-                setFormErrors({
-                    ...formErrors,
-                    [event.target.name]: error.formErrors
-                })
-            });
-    }   
+        setFormState(initialFormValues);
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  };
 
-        const formSubmit = event => {
-            event.preventDefault()
-            axios.post('https://reqres.in/api/users', formState)
-            .then((response) => {
-                setUsers([...users, response.data])
-                console.log(response.data)
+  const onInputChange = (event) => {
+    event.persist();
+    const newFormData = {
+      ...formState,
+      [event.target.name]:
+        event.target.type === "submit"
+          ? event.target.submit
+          : event.target.value,
+    };
+    validateChange(event);
+    setFormState(newFormData);
+  };
 
-                setFormState(initialFormValues)
-            })
-            .catch((error) => {
-                console.log(error.response)
-            });
-        };
+  return (
+    <StyledForm className="form-container" onSubmit={formSubmit}>
+      <label htmlFor="username">
+        <input
+          id="username"
+          value={formState.username}
+          type="text"
+          name="username"
+          placeholder="Username"
+          onChange={onInputChange}
+        />
+      </label>
 
-        const onInputChange = (event) => {
-            event.persist();
-            const newFormData = {
-                ...formState,
-                [event.target.name]: 
-                event.target.type === 'submit' ? event.target.submit : event.target.value
-            }
-            validateChange(event)
-            setFormState(newFormData)
-        }
+      <label htmlFor="password">
+        <input
+          id="password"
+          value={formState.password}
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={onInputChange}
+        />
+      </label>
 
-    return (
-
-        <form className='form-container' onSubmit={formSubmit}>
-            <label htmlFor='username'> Username: &nbsp;
-                <input id='username'
-                    value={formState.username}
-                    type='text'
-                    name='username'
-                    placeholder='Username'
-                    onChange={onInputChange}
-                />
-            </label>
-            <label htmlFor='password'> Password: &nbsp;
-                <input id='password'
-                    value={formState.password}
-                    type='password'
-                    name='password'
-                    placeholder='Password'
-                    onChange={onInputChange}
-                />
-            </label>
-            <button id='submitBtn' disabled={buttonDisabled} type='submit'>Submit</button>
-        </form>
-    )
+      <SubmitButton id="submitBtn" disabled={buttonDisabled} type="submit">
+        Login
+      </SubmitButton>
+    </StyledForm>
+  );
 }
 
 export default Login;
