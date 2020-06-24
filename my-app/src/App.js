8631292a8beaae2feee75c3ae90cd89271b77e9e
Wayne <wabbruscatoorr@gmail.com>
2020-06-24 18:28:44 -0400

    updating App file with Context.Provider

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,6 +7,8 @@ import Header from "./Component/Header";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./Component/Login";
 
+import { Context } from "./context.js/Context";
+
 const WrapperDiv = styled.div`
   font-family: sans-serif;
   text-align: center;
@@ -15,26 +17,26 @@ const WrapperDiv = styled.div`
 //import { Login } from "./Component/Login"; wayne
 //import { Register } from "./Component/Register"; wayne
 
-import { axiosWithAuth } from "./utils/axiosWithAuth";
 //import { Context } from "./context.js/Context"; wayne
 
 const App = () => {
-  const [listings] = useState(axiosWithAuth);
+  const [listings] = useState(/*DATA*/);
   return (
     <div className="App">
-      <Router>
-        <WrapperDiv>
-          <Header />
-          <br />
-          <Login />
-          {/*<Route></Route*/}
-        </WrapperDiv>
-      </Router>
+      <Context.Provider value={listings}>
+        <Router>
+          <WrapperDiv>
+            <Header />
+            <br />
+            <Login />
+            {/*<Route></Route*/}
+          </WrapperDiv>
+        </Router>
 
-      <FormContainer>
-        <Form />
-      </FormContainer>
-
+        <FormContainer>
+          <Form />
+        </FormContainer>
+      </Context.Provider>
       {/* <Context.Provider value={listings}> wayne
         <Route exact path="/">
           <Login />
